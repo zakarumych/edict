e1b4de9cfd2aa2ada66bccd83df730983fa636d9
Zakarum <zakarumych@ya.ru>
2022-04-26 13:03:40 +0400

    Add method to construct default Tracks value

--- src/world/tracks.rs
+++ src/world/tracks.rs
@@ -1,8 +1,16 @@
 /// Value to remember which modifications was already iterated over,
 /// and see what modifications are new.
-#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Clone, Debug, Default, PartialEq, Eq, PartialOrd, Ord)]
 #[allow(missing_copy_implementations)]
 #[repr(transparent)]
 pub struct Tracks {
     pub(crate) epoch: u64,
+}
+
+impl Tracks {
+    /// Returns new `Tracks` instance
+    /// that would consider any change to be new.
+    pub const fn new() -> Self {
+        Tracks { epoch: 0 }
+    }
 }

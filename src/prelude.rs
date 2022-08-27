8fd77a64f050ecc29d104d112efbd434216570b4
Zakarum <zakarumych@ya.ru>
2022-08-27 15:13:01 +0300

    Improved prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,10 +1,12 @@
 //! A prelude module. Reexports types and traits, enough to start using [`edict`]
 
 pub use crate::{
+    action::ActionEncoder,
     bundle::{Bundle, DynamicBundle, EntityBuilder},
     component::Component,
     entity::EntityId,
-    query::{PhantomQuery, Query, QueryIter},
-    relation::Relation,
+    query::{Alt, Modified, PhantomQuery, Query, QueryIter},
+    relation::{QueryRelated, QueryRelation, QueryRelationTo, Relation, WithRelationTo},
+    system::{IntoSystem, Res, ResMut, ResMutNoSend, ResNoSync, State, System},
     world::{EntityError, MissingComponents, NoSuchEntity, QueryOneError, QueryRef, World},
 };
